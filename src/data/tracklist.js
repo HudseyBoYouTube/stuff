c7ab0f9de93ec9dea97ba8c649224105697049f4
HudseyBoYouTube <ktmcmwkzpt@privaterelay.appleid.com>
2026-03-23 16:58:01 -0400

    Update tracklist.js

--- src/data/tracklist.js
+++ src/data/tracklist.js
@@ -5,10 +5,8 @@ export const myLibrary = [
     title: "Time", 
     artist: "Pink Floyd", 
     album: "Dark Side of the Moon",
-    // 1. Start with /music/ 
-    // 2. Add the EXACT filename from GitHub 
-    // 3. End with .mp3
-    url: "/music/Time-Pink-Floyd-(Official-Audio).mp3", 
+    // Change .mp3 to .mp4 so the browser finds the right file!
+    url: "/music/Time-Pink-Floyd-(Official-Audio).mp4", 
     isClean: true 
   }
 ];
