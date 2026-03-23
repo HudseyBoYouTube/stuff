a039b201c3e60de48f568d2aed632da83d63e0d2
HudseyBoYouTube <ktmcmwkzpt@privaterelay.appleid.com>
2026-03-23 16:46:03 -0400

    Create tracklist.js

---
+++ src/components/tracklist.js
@@ -0,0 +1,33 @@
+// src/data/tracklist.js
+
+export const myLibrary = [
+  { 
+    title: "Time", 
+    artist: "Pink Floyd", 
+    album: "Dark Side of the Moon",
+    url: "/music/time.mp3", 
+    isClean: true 
+  },
+  { 
+    title: "American Idiot", 
+    artist: "Green Day", 
+    album: "American Idiot",
+    url: "/music/american-idiot.mp3",
+    isClean: false 
+  },
+  { 
+    title: "Immortals", 
+    artist: "Fall Out Boy", 
+    album: "American Beauty/American Psycho",
+    url: "/music/immortals.mp3",
+    isClean: true 
+  },
+  { 
+    title: "Centuries", 
+    artist: "Fall Out Boy", 
+    album: "American Beauty/American Psycho",
+    url: "/music/centuries.mp3",
+    isClean: true 
+  }
+  // You can keep adding more from your big list here!
+];
