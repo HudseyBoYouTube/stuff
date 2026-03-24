4218dfc4774cd529db6ae1c4974a005cba08dd71
HudseyBoYouTube <ktmcmwkzpt@privaterelay.appleid.com>
2026-03-24 11:23:35 -0400

    Update tracklist.js

--- src/components/tracklist.js
+++ src/components/tracklist.js
@@ -2,34 +2,17 @@
 
 export const tracklist = [
   { 
-    title: "Time", 
-    artist: "Pink Floyd", 
-    album: "Dark Side of the Moon",
-    // Cleaned up the path to match your simplified file
-    url: "/music/time.mp3", 
-    isClean: true 
-  },
-  { 
     title: "American Idiot", 
     artist: "Green Day", 
     album: "American Idiot",
-    // Matches the filename "american-idiot.mp3"
-    url: "/music/american-idiot.mp3",
+    url: "/music/american-idiot.mp3", 
     isClean: false 
   },
   { 
-    title: "Immortals", 
-    artist: "Fall Out Boy", 
-    album: "American Beauty/American Psycho",
-    url: "/music/immortals.mp3",
-    isClean: true 
+    title: "Back In Black", 
+    artist: "AC/DC", 
+    album: "Back In Black",
+    url: "/music/back-in-black.mp3",
+    isClean: true
   },
-  { 
-    title: "Centuries", 
-    artist: "Fall Out Boy", 
-    album: "American Beauty/American Psycho",
-    url: "/music/centuries.mp3",
-    isClean: true 
-  }
-  // You can keep adding more from your big list here!
-];
+  ];
