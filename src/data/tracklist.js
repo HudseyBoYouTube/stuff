5c408cad67f220e5c4c143aba6770f8d48131f7a
HudseyBoYouTube <ktmcmwkzpt@privaterelay.appleid.com>
2026-03-23 20:47:08 -0400

    Update tracklist.js

--- src/data/tracklist.js
+++ src/data/tracklist.js
@@ -5,8 +5,16 @@ export const myLibrary = [
     title: "Time", 
     artist: "Pink Floyd", 
     album: "Dark Side of the Moon",
-    // Change .mp3 to .mp4 so the browser finds the right file!
-    url: "/music/Time-Pink-Floyd-(Official-Audio).mp4", 
+    // Points to the new simplified filename in your public/music folder
+    url: "/music/time.mp3", 
     isClean: true 
+  },
+  { 
+    title: "American Idiot", 
+    artist: "Green Day", 
+    album: "American Idiot",
+    // Make sure your file in public/music/ is named american-idiot.mp3
+    url: "/music/American Idiot - Green Day.mp3", 
+    isClean: false 
   }
 ];
