67c8d128bedea55123114dedc18e116f178e78cd
HudseyBoYouTube <ktmcmwkzpt@privaterelay.appleid.com>
2026-04-08 19:13:29 -0400

    Update useAchievements.js

--- src/hooks/useAchievements.js
+++ src/hooks/useAchievements.js
@@ -5,21 +5,23 @@ export function useAchievements(userData) {
 
   useEffect(() => {
     if (!userData) return;
+    
     const unlocked = [];
     
-    // Check for "First Blood"
     if (userData.times && Object.keys(userData.times).length > 0) {
-      unlocked.push('first_game');
+      unlocked.push({
+        id: 'first_game',
+        title: 'Getting Started',
+        description: 'Play your first game!'
+      });
     }
 
-    // Check for "Fashionista"
     if (userData.themeChanges >= 5) {
-      unlocked.push('styler');
-    }
-
-    // Check for "The Collector"
-    if (userData.favs && userData.favs.length >= 10) {
-      unlocked.push('collector');
+      unlocked.push({
+        id: 'styler',
+        title: 'Fashionista',
+        description: 'Change the theme 5 times!'
+      });
     }
 
     setAchievements(unlocked);
