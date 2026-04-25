9825f06489ea9193290bcbde58adb6a2d7d7dd1f
HudseyBoYouTube <ktmcmwkzpt@privaterelay.appleid.com>
2026-04-25 15:00:35 -0400

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,22 @@
 export const applyCloak = (isCloaked) => {
-  let link = document.querySelector("link[rel~='icon']");
-  
+  let link = document.querySelector("link[rel*='icon']");
   if (!link) {
     link = document.createElement('link');
-    link.rel = 'icon';
+    link.rel = 'shortcut icon';
     document.getElementsByTagName('head')[0].appendChild(link);
   }
 
   if (isCloaked) {
-    link.href = 'https://www.google.com/images/branding/product/ico/googleg_lodp.ico';
+    link.type = 'image/x-icon';
+    link.href = 'https://www.google.com/favicon.ico';
     document.title = 'Google';
   } else {
+    link.type = 'image/png';
     link.href = 'https://img.icons8.com/color/32/capybara.png'; 
     document.title = 'Capybara Science';
   }
+
+  const h = document.getElementsByTagName('head')[0];
+  h.removeChild(link);
+  h.appendChild(link);
 };
